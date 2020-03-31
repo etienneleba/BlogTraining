1ce1ae0952b62b0cea689fde5a921272a745971f
etienneleba <etienne@lebarillier.fr>
2020-03-31 16:09:37 +0200

    fixtures changes

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,17 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $loader = new NativeLoader();
+        $objectSet = $loader->loadFile(__DIR__.'/usersFixture.yaml')->getObjects();
+        foreach ($objectSet as $object) {
+            $manager->persist($object);
+        }
         $manager->flush();
     }
 }
