ba0593d5597d573730b2098d181b85ff8cdecaae
etienneleba <etienne@lebarillier.fr>
2020-03-31 16:08:12 +0200

    repo changes

--- src/Repository/AlternativeRepository.php
+++ src/Repository/AlternativeRepository.php
@@ -7,16 +7,42 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
- * @method Alternative|null find($id, $lockMode = null, $lockVersion = null)
- * @method Alternative|null findOneBy(array $criteria, array $orderBy = null)
+ * @method null|Alternative find($id, $lockMode = null, $lockVersion = null)
+ * @method null|Alternative findOneBy(array $criteria, array $orderBy = null)
  * @method Alternative[]    findAll()
  * @method Alternative[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Alternative[]    findByTypeAndContentType(int $typeId = null, int $contentTypeId = null)
  */
 class AlternativeRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Alternative::class);
+    }
+
+    public function findAll()
+    {
+        return  $this->findBy([], ['created_at' => 'DESC']);
+    }
+
+    public function findByTypeAndContentType(int $typeId = null, int $contentTypeId = null)
+    {
+        $query = $this->createQueryBuilder('a');
+        if ($typeId) {
+            $query->andWhere('a.type = :typeId')
+                ->setParameter('typeId', $typeId)
+            ;
+        }
+        if ($contentTypeId) {
+            $query->andWhere('a.contentType = :contentTypeId')
+                ->setParameter('contentTypeId', $contentTypeId)
+            ;
+        }
+
+        return $query->orderBy('a.created_at', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
     }
 
     // /**
